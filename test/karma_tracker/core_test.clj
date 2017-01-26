3780520757e107810730efd28094737c6b7e07d6
Fabio Volpe <volpefabio@gmail.com>
2017-01-26 14:37:42 +0000

    minimal lein project

---
+++ test/karma_tracker/core_test.clj
@@ -0,0 +1,7 @@
+(ns karma-tracker.core-test
+  (:require [clojure.test :refer :all]
+            [karma-tracker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
