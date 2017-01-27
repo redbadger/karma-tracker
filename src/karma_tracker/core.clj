d49df5e44fffa700a8572fedba17b4373a2d9db5
Andrew Haines <andrew.haines@red-badger.com>
2017-01-27 16:04:24 +0000

    Demonstrate caching of API requests
    Run (demo-caching) in the REPL to see the effects!

--- src/karma_tracker/core.clj
+++ src/karma_tracker/core.clj
@@ -1,5 +1,16 @@
 (ns karma-tracker.core
+  (:require [tentacles.events :as events]
+            [clj-http.client :as http]
+            [karma-tracker.cache :refer [wrap-cache]])
   (:gen-class))
+
+(defn demo-caching
+  "All HTTP calls made inside the `http/with-middleware` function are cached.
+  This code should result in 10 HTTP calls and hence 10 cache files being generated.
+  A second call to the function will return much more quickly because the responses will be read from disk."
+  []
+  (http/with-middleware (conj http/default-middleware wrap-cache)
+    (count (events/user-events "redbadger" {:all-pages true}))))
 
 (defn -main
   "I don't do a whole lot ... yet."
