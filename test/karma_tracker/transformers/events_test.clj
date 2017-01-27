68ec2d22da8adbdedd4a2d49b1fe7f6d1b1b084f
mveritym <mveritym@gmail.com>
2017-01-27 12:04:01 +0000

    Fixing edn file loading

--- test/karma_tracker/transformers/events_test.clj
+++ test/karma_tracker/transformers/events_test.clj
@@ -1,11 +1,12 @@
 (ns karma-tracker.transformers.events-test
   (:require [clojure.test :refer :all]
             [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [karma-tracker.transformers.events :refer :all]))
 
 (deftest issue-event-transform
   (testing "Transforms a Github IssueEvent into an event map"
-    (let [resource-str "test/karma_tracker/resources/event-transformer.edn"
-          input-issue (edn/read-string (slurp resource-str))
+    (let [input-file (io/file (io/resource  "event-transformer.edn"))
+          input-issue (edn/read-string (slurp input-file))
           expected-event {:action "closed" :type "issue"}]
       (is (= expected-event (transform-to-event input-issue))))))
