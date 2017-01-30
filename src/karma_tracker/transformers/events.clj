867c1665338d8f383269642b6e24ff066220472b
mveritym <mveritym@gmail.com>
2017-01-30 14:39:17 +0000

    Adding pull request comment event to handled events

--- src/karma_tracker/transformers/events.clj
+++ src/karma_tracker/transformers/events.clj
@@ -1,6 +1,7 @@
 (ns karma-tracker.transformers.events)
 
-(def event-types {:IssuesEvent "issue"})
+(def event-types {:IssuesEvent "issue"
+                  :PullRequestReviewCommentEvent :pull-request-comment})
 
 (defn get-event-type [type]
   (get event-types (keyword type)))
--- test/karma_tracker/transformers/events_test.clj
+++ test/karma_tracker/transformers/events_test.clj
@@ -14,3 +14,14 @@
                           :user {:login "gaearon" :id 810438}
                           :created-at "2017-01-26T15:50:20Z"}]
       (is (= expected-event (transform-to-event input-issue))))))
+
+(deftest pr-comment-event-transform
+  (testing "Transforms a Github PullRequestReviewCommentEvent into an event map"
+    (let [input-file (-> "pr-comment-event.edn" io/resource io/file)
+          input-pr-comment (-> input-file slurp edn/read-string)
+          expected-pr-comment {:action "created"
+                               :type :pull-request-comment
+                               :repo {:name "redbadger/karma-tracker" :id 80008322}
+                               :user {:login "mveritym" :id 1009524}
+                               :created-at "2017-01-30T11:35:09Z"}]
+      (is (= expected-pr-comment (transform-to-event input-pr-comment))))))
