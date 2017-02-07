ba80dba5be61f0ff1cdd211bdec1b21162ef4b79
Fabio Volpe <volpefabio@gmail.com>
2017-02-07 11:29:49 +0000

    github client tests

--- test/karma_tracker/github_test.clj
+++ test/karma_tracker/github_test.clj
@@ -3,7 +3,8 @@
             [karma-tracker.github :refer :all]
             [tentacles
              [events :as events]
-             [orgs :as orgs]]))
+             [orgs :as orgs]
+             [repos :as repos]]))
 
 (defn fake-call [& args] args)
 
@@ -22,4 +23,20 @@
 
     (testing "user performed events api call"
       (with-redefs [events/performed-events fake-call]
-        (is (= ["hacker" opts] (performed-events conn "hacker")))))))
+        (is (= ["hacker" opts] (performed-events conn "hacker")))))
+
+    (testing "repository languages"
+      (with-redefs [repos/languages fake-call]
+        (is (= ["hacker" "magic-app" opts] (repo-languages conn "hacker" "magic-app")))))))
+
+(deftest organisation-performed-events-test
+  (with-redefs [orgs/members            (fn [& args]
+                                          [{:login "hacker1"}
+                                           {:login "hacker2"}])
+                events/performed-events (fn [login _]
+                                          [(str "event1 " login)
+                                           (str "event2 " login)])]
+    (let [conn (new-connection "hacker" "H4CK3RT0CK3N" {})]
+      (organisation-performed-events conn "redbadger")
+      (is (= (organisation-performed-events conn "redbadger")
+             '("event1 hacker1" "event2 hacker1" "event1 hacker2" "event2 hacker2"))))))
