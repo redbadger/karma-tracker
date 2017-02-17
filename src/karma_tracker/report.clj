fb4699c4116179d0cafb8e66e00d120089702140
Fabio Volpe <volpefabio@gmail.com>
2017-02-17 10:45:52 +0000

    report command line

--- src/karma_tracker/report.clj
+++ src/karma_tracker/report.clj
@@ -1,8 +1,23 @@
 (ns karma-tracker.report
-  (:require [cljstache.core :refer [render-resource]]))
+  (:require [cljstache.core :refer [render-resource]]
+            [karma-tracker.events-repository :refer [get-events-for-month]]
+            [karma-tracker.events :refer [transform]]
+            [karma-tracker.aggregation :refer [aggregate]]
+            [karma-tracker.augmentation :refer [augment]]))
 
-(defn generate-report [report-data]
+(defn render [report-data]
   (render-resource "report/template.mustache" report-data))
 
-(defn save-report [report]
+(defn save [report]
   (spit "report.html" report))
+
+(defn get-data [github-conn events-storage year month]
+  (->> (get-events-for-month events-storage year month)
+       (sequence transform)
+       aggregate
+       (augment github-conn)))
+
+(defn make-report [github-conn events-storage year month]
+  (-> (get-data github-conn events-storage year month)
+      render
+      save))
