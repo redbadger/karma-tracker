c4bc8905b161bd651f06f7d2eb37787350876df4
Andrew Haines <andrew.haines@red-badger.com>
2017-03-09 16:04:10 +0000

    Sort users case-insensitively

--- src/karma_tracker/query/users.clj
+++ src/karma_tracker/query/users.clj
@@ -1,5 +1,9 @@
-(ns karma-tracker.query.users)
+(ns karma-tracker.query.users
+  (:require [clojure.string :as string]))
 
 (defn users [events]
   "Aggregates events into a set of users login names"
-  (->> events (map :user) set sort))
+  (->> events
+       (map :user)
+       set
+       (sort-by string/lower-case)))
