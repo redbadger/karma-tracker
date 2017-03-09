40567f7cbfa8000d9d766762536276db7d130018
Andrew Haines <andrew.haines@red-badger.com>
2017-03-09 10:26:46 +0000

    Use array-map to preserve order

--- ui/src/cljs/karma_tracker_ui/db.cljs
+++ ui/src/cljs/karma_tracker_ui/db.cljs
@@ -49,19 +49,19 @@
                          :comments {:count 80, :change 1.20}
                          :total {:count 157, :change 0.50}}
 
-   :repositories {"redbadger/website-honestly" {:commits {:count 123}
-                                                :pull-requests {:count 10}
-                                                :issues {:count 5}
-                                                :comments {:count 14}
-                                                :total {:count 152}}
-                  "reactjs/react-redux" {:commits {:count 5}
-                                         :pull-requests {:count 3}
-                                         :issues {:count 10}
-                                         :comments {:count 58}
-                                         :total {:count 76}}}
+   :repositories (array-map "redbadger/website-honestly" {:commits {:count 123}
+                                                          :pull-requests {:count 10}
+                                                          :issues {:count 5}
+                                                          :comments {:count 14}
+                                                          :total {:count 152}}
+                            "reactjs/react-redux" {:commits {:count 5}
+                                                   :pull-requests {:count 3}
+                                                   :issues {:count 10}
+                                                   :comments {:count 58}
+                                                   :total {:count 76}})
 
-   :languages {"JavaScript" 9000
-               "Clojure" 5000}
+   :languages (array-map "JavaScript" 9000
+                         "Clojure" 5000)
 
    :contributors [{:username "gregstewart", :avatar "https://avatars1.githubusercontent.com/u/474076?v=3&s=240"}
                   {:username "haines", :avatar "https://avatars3.githubusercontent.com/u/785641?v=3&s=240"}
