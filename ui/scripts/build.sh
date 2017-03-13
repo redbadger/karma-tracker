2d3e39eda155ec341fbdc7161d2bc9fae4636abd
Andrew Haines <andrew.haines@red-badger.com>
2017-03-13 18:12:20 +0000

    Tag built Docker image

--- ui/scripts/build.sh
+++ ui/scripts/build.sh
@@ -4,4 +4,5 @@ lein do clean, cljsbuild once production
 
 brunch build --production
 
-docker build .
+docker build --tag karma-tracker/ui:latest .
+docker tag karma-tracker/ui:latest 458707459747.dkr.ecr.eu-west-1.amazonaws.com/karma-tracker/ui:latest
